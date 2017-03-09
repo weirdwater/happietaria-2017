2cc3dac64fd53f76ed0ba1b6b7092666a2eb4038
Arjo Bruijnes <arjo@weirdwater.net>
2017-03-10 00:21:05 +0100

    Update theme metadata and remove example code

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
 
 wp_enqueue_style( 'happietaria-2017-style', get_template_directory_uri() . '/static/happietaria-2017.css' );
 
+// Timber Configuration
+
 if ( ! class_exists( 'Timber' ) ) {
 	add_action( 'admin_notices', function() {
 		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php') ) . '</a></p></div>';
@@ -38,24 +40,16 @@ class StarterSite extends TimberSite {
 		//this is where you can register custom taxonomies
 	}
 
-	function add_to_context( $context ) {
-		$context['foo'] = 'bar';
-		$context['stuff'] = 'I am a value set in your functions.php file';
-		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
-		$context['menu'] = new TimberMenu();
-		$context['site'] = $this;
-		return $context;
-	}
-
-	function myfoo( $text ) {
-		$text .= ' bar!';
-		return $text;
-	}
+	function add_to_context( $context )
+    {
+        $context['menu'] = new TimberMenu();
+        $context['site'] = $this;
+        return $context;
+    }
 
 	function add_to_twig( $twig ) {
 		/* this is where you can add your own functions to twig */
 		$twig->addExtension( new Twig_Extension_StringLoader() );
-		$twig->addFilter('myfoo', new Twig_SimpleFilter('myfoo', array($this, 'myfoo')));
 		return $twig;
 	}
 
