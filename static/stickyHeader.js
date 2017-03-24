791e06d8480bad4246db13b643bae17736ce5f71
Arjo Bruijnes <arjo@weirdwater.net>
2017-03-24 14:38:37 +0100

    Toggle menu on click

--- static/stickyHeader.js
+++ static/stickyHeader.js
@@ -1,6 +1,11 @@
 (function() {
-    const header = document.getElementById('header');
-    const newsBar = document.getElementById('newsBar');
+    const header = document.getElementById('header'),
+        newsBar = document.getElementById('newsBar'),
+        stickyHeader = document.getElementById('stickyHeader'),
+        menuToggle = stickyHeader.querySelector('#menuToggle');
+
+    const strCloseMenu = 'Sluit menu',
+        strOpenMenu = 'Open menu';
 
     (function manageStickyHeader() {
         const newsBarBottomPosition = newsBar.getBoundingClientRect().bottom;
@@ -8,4 +13,22 @@
         header.dataset.fixed = newsBarBottomPosition < 0;
         window.requestAnimationFrame(manageStickyHeader);
     })();
+
+    function manageMenuToggle() {
+        const menuIsOpen = stickyHeader.dataset.open;
+        switch(menuIsOpen) {
+            case 'true':
+                // close menu
+                stickyHeader.dataset.open = false;
+                menuToggle.innerText = strOpenMenu;
+                break;
+            default:
+                // open menu
+                stickyHeader.dataset.open = true;
+                menuToggle.innerText = strCloseMenu;
+        }
+    }
+
+    menuToggle.addEventListener('click', manageMenuToggle);
+
 })();
